dc6608b4c9aef89c4f5d1b6049ece86e1a5860b6
gigo83 <134645012+gigo83@users.noreply.github.com>
2025-06-13 23:26:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 # Flask Setup
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///songs.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 db = SQLAlchemy(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
